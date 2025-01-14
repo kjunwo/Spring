0ffe680c2b43b36c6c2e871d12a9638279a839da
kangjunwoo <kangjunwoo02@naver.com>
2025-01-14 16:20:45 +0900

    삭제

--- driverapp/src/main/java/com/dw/driverapp/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dw.driverapp.repository;
-
-import com.dw.driverapp.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
