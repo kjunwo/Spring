e56c022ea809df4227c3e2b4b5668acde6f74811
kangjunwoo <kangjunwoo02@naver.com>
2025-01-17 09:04:24 +0900

    수정

---
+++ 문서/driverapp/src/main/java/com/dw/driverapp/model/Comment.java
@@ -0,0 +1,27 @@
+package com.dw.driverapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "답글")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name="user_name")
+    private User user;
+    @Column(name = "comment", nullable = false, columnDefinition = "TEXT") // 65535 byte
+    private String comment;
+    @ManyToOne
+    @JoinColumn(name="board_id")
+    private Board board;
+}
